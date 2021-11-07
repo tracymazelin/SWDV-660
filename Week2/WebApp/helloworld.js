4ff89ac56cd460408a27c310ebb5b55baa2ca5c3
Tracy Mazelin <tracy.mazelin@fellowshipone.com>
2021-11-07 12:38:33 -0500

    Update helloworld

--- Week2/WebApp/helloworld.js
+++ Week2/WebApp/helloworld.js
@@ -1,14 +1,14 @@
 var http = require("http")
 
-http.createServer(function (request, response) {
+http.createServer(function(request, response) {
 
-   // Send the HTTP header
-   // HTTP Status: 200 : OK
-   // Content Type: text/plain
-   response.writeHead(200, {'Content-Type': 'text/plain'})
+    // Send the HTTP header
+    // HTTP Status: 200 : OK
+    // Content Type: text/plain
+    response.writeHead(200, { 'Content-Type': 'text/plain' })
 
-   // Send the response body as "Hello World"
-   response.end('Ayup mi duck!\n')
+    // Send the response body as "Hello World Updated"
+    response.end('Hello World Updated\n')
 }).listen(3000)
 
 // Console will print the message
